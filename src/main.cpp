cd56c4548bda7b979665fe66069ecc543a49f412
AlvarezRoss <alvarezoteromartin@gmail.com>
2026-01-10 21:14:07 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  // TODO: Uncomment the code below to pass the first stage
-  // std::cout << "$ ";
+  std::cout<<"$ ";
+  return 0;
 }
