4f3cb0c05fadbb09e29c6d8ad3a855a1188d5d52
AlvarezRoss <alvarezoteromartin@gmail.com>
2026-02-02 18:59:06 +0000

    codecrafters submit [skip ci]

--- src/parser.cpp
+++ src/parser.cpp
@@ -17,7 +17,7 @@ std::string Parser::GetFullArgumentString(std::string& arg, char delimiter) {
     std::string argumentString = arg.substr(begin+1,std::string::npos);
     return argumentString;
 }
-// Tokenizer supports ' on command lines
+// Tokenizer supports ' and '\' on command lines  
 std::vector<std::string>* Parser::TokenizeFullCommand(std::string& command,char delimiter) {
     if (tokens.size() > 0) tokens.clear();
     bool inSingleQuotes = false;
@@ -28,14 +28,15 @@ std::vector<std::string>* Parser::TokenizeFullCommand(std::string& command,char 
     int argLen = command.length();
     for (int i = 0; i <= argLen; i++){
         
-        if (command[i] == '\'' && !inDoubleQuotes) {
+        if (command[i] == '\'' && !inDoubleQuotes) {// skips contiguous single quotes
             if (command[i+1] == '\'') {
                 i++;
                 continue;
             }   
+
             if (inSingleQuotes){
-                std::string tok = std::string(command.data() + begin, i - begin);
-                tok.erase(std::remove(tok.begin(),tok.end(),'\''),tok.end());
+                std::string tok = std::string(command.data() + begin, i - begin); // Creates a string from the command and the begin and i delimiters
+                tok.erase(std::remove(tok.begin(),tok.end(),'\''),tok.end()); // removes any single quotes from the string
                 if (word != "") {
                     word.append(tok);
                     tokens.emplace_back(word);
@@ -50,26 +51,34 @@ std::vector<std::string>* Parser::TokenizeFullCommand(std::string& command,char 
             inSingleQuotes = !inSingleQuotes;
         }
         if (command[i] == '\"' && !inSingleQuotes) {
-            if (command[i+1] == '\"'){
+            
+            if (command[i+1] == '\"'){ // skips over contiguous double quotes
                 i++;
                 continue;
             }
-            if (inDoubleQuotes) {
-                std::string tok = std::string(command.data() + begin, i - begin);
-                tok.erase(std::remove(tok.begin(),tok.end(),'\"'), tok.end());
-                if (word != "") {
-                    word.append(tok);
-                    tokens.emplace_back(word);
-                }
-                else {
-                    tokens.emplace_back(tok);
-                }
+            if (i + 1 >= argLen || command[i+1] == ' '){
+                tokens.emplace_back(word);
+                word = "";
+            }
+            inDoubleQuotes = !inDoubleQuotes;
+            begin = i + 1; // begin in incremented to make sure single quote handle is correct
+            i++; //skips the " when encountered
+
+        }
+        if (inDoubleQuotes) {
+                if (command[i] == '\\') { // if a \ is found it appends the next char
+                        if (command[i+1] == '\"' || command[i+1] == '\\'){
+                            word.append(1,command[i+1]);
+                            i++;
+                            continue;
+                        }
+                    }
+                else{
+                    word.append(1,command[i]);
+                    continue;
+                } 
                 
             }
-            begin = i + 1;
-            
-            inDoubleQuotes = !inDoubleQuotes;
-        }
         if (!inDoubleQuotes && !inSingleQuotes) {
                 
                 if (command[i] == delimiter || i >= argLen) {
@@ -94,16 +103,13 @@ std::vector<std::string>* Parser::TokenizeFullCommand(std::string& command,char 
                     }
                     else {
                         i++;
-                        //begin = i+1;
                     }
                     continue;
                 } 
                 else if (command[i] != ' ' && command[i] != '\"' && command[i] != '\''){
                     word.append(1,command[i]);
-                    //begin = i+1;
                     continue;
                 } 
-            //begin = i+1;
                 
         }
 
